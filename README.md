117f37950c3a37c97afb36ad71383a979d4dc092
wuyr <2309474763@qq.com>
2019-04-24 16:58:43 +0800

    Release 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# LitePager
+##  LitePager，一个轻量级的ViewPager，仿新版网易云歌单广场
+### 博客详情： 敬请期待。。。
+
+### 使用方式:
+#### 添加依赖：
+```
+implementation 'com.wuyr:litepager:1.0.0'
+```
+
+### APIs:
+|Method|Description|
+|------|-----------|
+|addViews(int... layouts)|批量添加子View|
+|addViews(View... views)|批量添加子View|
+|setSelection(View target)|选中指定子View|
+|setSelection(int index)|根据索引选中子View|
+|setOrientation(int orientation)|设置方向(默认: ORIENTATION_HORIZONTAL):<br>**ORIENTATION_HORIZONTAL**(水平)<br>**ORIENTATION_VERTICAL**(垂直)|
+|setFlingDuration(long duration)|设置动画的时长|
+|setMaxScale(float maxScale)|设置**最大**缩放比例|
+|setMinScale(float minScale)|设置**最小**缩放比例|
+|setMaxAlpha(float maxAlpha)|设置**最大**不透明度|
+|setMinAlpha(float minAlpha)|设置**最小**不透明度|
+|setOnScrollListener(OnScrollListener listener)|设置滚动状态监听:<br>**STATE_IDLE**(静止状态)<br>**STATE_DRAGGING_LEFT**(向左拖动)<br>**STATE_DRAGGING_RIGHT**(向右拖动)<br>**STATE_DRAGGING_TOP**(向上拖动)<br>**STATE_DRAGGING_BOTTOM**(向下拖动)<br>**STATE_SETTLING_LEFT**(向左调整)<br>**STATE_SETTLING_RIGHT**(向右调整)<br>**STATE_SETTLING_TOP**(向上调整)<br>**STATE_SETTLING_BOTTOM**(向下调整)<br>|
+|setOnItemSelectedListener(SelectedListener listener) |设置子View被选中的监听|
+|getSelectedChild() |获取当前选中的子View|
+
+### Attributes:
+|Name|Format|Description|
+|----|-----|-----------|
+|orientation|enum (默认: horizontal)<br>**horizontal**(水平)<br>**vertical**(垂直)|方向|
+|flingDuration|integer|动画时长|
+|maxScale|float (默认: 1)|**最大**缩放比例|
+|minScale|float (默认: 0.8)|**最小**缩放比例|
+|maxAlpha|float (默认: 1)|**最大**不透明度|
+|minAlpha|float (默认: 0.4)|**最小**不透明度|
+
+### 添加子View方式：
+#### XML
+
+```xml
+    <com.wuyr.litepager.LitePager
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
+
+        <View
+            android:layout_width="150dp"
+            android:layout_height="200dp"
+            android:background="#F00" />
+
+        <View
+            android:layout_width="150dp"
+            android:layout_height="200dp"
+            android:background="#0F0"/>
+
+        <View
+            android:layout_width="150dp"
+            android:layout_height="200dp"
+            android:background="#00F"/>
+    </com.wuyr.litepager.LitePager>
+```
+
+#### 或者
+
+```java
+    LitePager litePager = ...;
+    View child1 = ...;
+    View child2 = ...;
+    View child3 = ...;
+    //直接添加子View
+    litePager.addViews(child1, child2, child3);
+    
+    //或者通过布局添加
+    litePager.addViews(R.layout.view_child1, R.layout.view_child2, R.layout.view_child3);
+```
+
+<br>
+
+### Demo下载: [app-debug.apk](https://github.com/wuyr/LitePager/raw/master/app-debug.apk)
+### 库源码地址： <https://github.com/Ifxcyr/LitePager>
+
+### 效果 (图1为网易云原效果)：
+![preview](https://github.com/wuyr/LitePager/raw/master/previews/preview1.gif) ![preview](https://github.com/wuyr/LitePager/raw/master/previews/preview2.gif)
+![preview](https://github.com/wuyr/LitePager/raw/master/previews/preview3.gif) ![preview](https://github.com/wuyr/LitePager/raw/master/previews/preview4.gif)
---
+++ app/src/main/java/com/wuyr/litepagertest/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.wuyr.litepagertest
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.wuyr.litepager.LitePager.*
+import kotlinx.android.synthetic.main.act_main_view.*
+
+/**
+ * @author wuyr
+ * @github https://github.com/wuyr/LitePager
+ * @since 2019-03-21 下午5:36
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_main_view)
+        initListeners()
+    }
+
+    private lateinit var mToast: Toast
+
+    @SuppressLint("ShowToast")
+    private fun initListeners() {
+        mToast = Toast.makeText(this,"", Toast.LENGTH_LONG)
+        litePager.setOnItemSelectedListener { onItemClick(it) }
+        litePager.setOnScrollListener {
+            status.text = getString(R.string.status, getString(when (it) {
+                STATE_IDLE -> R.string.idle
+                STATE_DRAGGING_LEFT -> R.string.dragging_left
+                STATE_DRAGGING_RIGHT -> R.string.dragging_right
+                STATE_DRAGGING_TOP -> R.string.dragging_top
+                STATE_DRAGGING_BOTTOM -> R.string.dragging_bottom
+                STATE_SETTLING_LEFT -> R.string.settling_left
+                STATE_SETTLING_RIGHT -> R.string.settling_right
+                STATE_SETTLING_TOP -> R.string.settling_top
+                STATE_SETTLING_BOTTOM -> R.string.settling_bottom
+                else -> return@setOnScrollListener
+            }))
+        }
+        orientationSwitch.setOnCheckedChangeListener { _, isChecked ->
+            litePager.setOrientation(if (isChecked) ORIENTATION_VERTICAL else ORIENTATION_HORIZONTAL)
+        }
+        val listener = object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                val value = progress / 100F
+                when (seekBar.id) {
+                    R.id.maxScaleBar -> litePager.setMaxScale(value)
+                    R.id.minScaleBar -> litePager.setMinScale(value)
+                    R.id.maxAlphaBar -> litePager.setMaxAlpha(1F - value)
+                    R.id.minAlphaBar -> litePager.setMinAlpha(1F - value)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        }
+        minScaleBar.setOnSeekBarChangeListener(listener)
+        minAlphaBar.setOnSeekBarChangeListener(listener)
+        maxScaleBar.setOnSeekBarChangeListener(listener)
+        maxAlphaBar.setOnSeekBarChangeListener(listener)
+    }
+
+    private fun onItemClick(view: View) {
+        mToast.setText("${(((view as ViewGroup).getChildAt(0) as ViewGroup)
+            .getChildAt(1) as TextView).text} selected")
+        mToast.show()
+    }
+
+    fun select0(view: View) {
+        litePager.setSelection(紫钗记)
+    }
+
+    fun select1(view: View) {
+        litePager.setSelection(再世红梅记)
+    }
+
+    fun select2(view: View) {
+        litePager.setSelection(帝女花)
+    }
+}
